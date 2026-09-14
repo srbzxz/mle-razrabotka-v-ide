ec607ad46af65586fe3f71cd6548fbffbdc1059d
Бекмамбетов Сергей Рустэмович <srbzxz@gmail.com>
2026-09-14 21:27:11 +0300

    feat(reporter): добавить метод show_report с базовой статистикой
    Вывод заголовка, количества строк и столбцов, количества
    и доли дубликатов (доля форматируется согласно percent_format).

--- src/reporter.py
+++ src/reporter.py
@@ -3,3 +3,14 @@ class DataFrameReporter:
         self.float_format = float_format
         self.percent_format = percent_format
         self.include_all = include_all
+
+    def show_report(self, df, title=None):
+        if title:
+            print(title)
+
+        print('Количество столбцов:', df.shape[1])
+        print('Количество строк:', df.shape[0])
+
+        duplicates = df.duplicated().sum()
+        print('Количество дубликатов:', duplicates)
+        print('Доля дубликатов:', format(duplicates / df.shape[0], self.percent_format))
